0b80739d6b4eed6b93a433d52c4274d4e5dc05ec
SongKyeongYong <ruuddyd@gmail.com>
2020-09-03 17:49:03 +0900

    init

---
+++ world/src/test/java/kr/ac/sejong/data_analysis/world/JSONworld.java
@@ -0,0 +1,14 @@
+package kr.ac.sejong.data_analysis.world;
+import org.json.*;
+
+public class JSONworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JSONObject jsonHello=new JSONObject();
+		jsonHello.put("message","MyJSONHelloWorld!");
+		System.out.println(jsonHello);
+
+	}
+
+}
---
+++ world/src/test/java/kr/ac/sejong/data_analysis/world/init.java
@@ -0,0 +1,10 @@
+package kr.ac.sejong.data_analysis.world;
+
+public class init {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
