4f09e941c2cde95c946ba40c84cf5f83e046ad56
SongKyeongYong <ruuddyd@gmail.com>
2020-10-29 20:20:46 +0900

    sample data

--- solution/src/main/java/kr/ac/sejong/data_analysis/solution/Application.java
+++ solution/src/main/java/kr/ac/sejong/data_analysis/solution/Application.java
@@ -1,7 +1,9 @@
  package kr.ac.sejong.data_analysis.solution;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,9 +17,22 @@ public class Application {
 		Statement stmt = connection.createStatement();
 		stmt.executeUpdate("CREATE OR REPLACE DATABASE DBPS");
 		stmt.executeUpdate("USE DBPS");
-		stmt.executeUpdate("CREATE OR REPLACE TABLE 사원 (사원번호 INTEGER, 직속상사번호 INTEGER, 이름 VARCHAR(50), 월급 DOUBLE,"
-				+ "직급 VARCHAR(50)) CHARACTER SET 'euckr';");
-		stmt.executeUpdate("INSERT INTO 사원 VALUES (1, null, '송경용', 50000, '사장')");
+		stmt.executeUpdate("CREATE OR REPLACE TABLE customer (NAME VARCHAR(50), CUSTOMER_ID INTEGER, LAST_VISIT DATE)");
+		
+		stmt.executeUpdate("INSERT INTO customer VALUES ('Jane Markham', 1, '2020-03-15')");
+		stmt.executeUpdate("INSERT INTO customer VALUES ('Louis Smith', 2, '2020-04-10')");
+		stmt.executeUpdate("INSERT INTO customer VALUES ('Woodrow Lang', 3, '2020-05-25')");
+		stmt.executeUpdate("INSERT INTO customer VALUES ('Dr.Johnson', 4, '2020-06-21')");
+
+		
+		ResultSet rs = stmt.executeQuery("SELECT * FROM customer");
+		while(rs.next()) {
+			String name = rs.getString(1);
+			int id = rs.getInt(2);
+			Date lv = rs.getDate(3);
+			
+			System.out.println(name + " " + id + " " + lv);
+		}
 		
 		stmt.close();
 		connection.close();
