a3c1edc53405839fcfdec3208f286060fa22bc61
SongKyeongYong <ruuddyd@gmail.com>
2020-11-05 12:06:17 +0900

    data insert

--- solution/src/main/java/kr/ac/sejong/data_analysis/solution/EmaillApp.java
+++ solution/src/main/java/kr/ac/sejong/data_analysis/solution/EmaillApp.java
@@ -4,11 +4,34 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLInvalidAuthorizationSpecException;
+import java.sql.Statement;
 
 public class EmaillApp {
 
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, SQLException {
+		
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306", "root", "1234");
+		}catch(SQLInvalidAuthorizationSpecException e){
+			e.printStackTrace();
+			System.out.println("PASSWORD ERROR");
+			System.exit(1);
+		}
+		
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate("CREATE OR REPLACE DATABASE dbp");
+		stmt.executeUpdate("USE dbp");
+		stmt.executeUpdate("CREATE OR REPLACE TABLE email (source INTEGER, destination INTEGER)");
+
+
+		
+		
 		BufferedReader r = new BufferedReader(
 				new FileReader("/Users/dragon/data.txt"));
 		
@@ -24,6 +47,8 @@ public class EmaillApp {
 			String[] arr = line.split("\t");
 			int left = Integer.parseInt(arr[0]);
 			int right = Integer.parseInt(arr[1]);
+			
+			stmt.executeUpdate("INSERT INTO email VALUES("+ left +", "+ right +")");
 
 			System.out.println(left + " -> " + right);
 		}
